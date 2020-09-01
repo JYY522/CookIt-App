192eb9851a64e15d6d9223f3877d1d0206d74ed5
JYY <jyy@JYYdeMBP.fios-router.home>
2020-09-01 00:11:12 -0400

    initial commit

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $50",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "$50 to $100",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "$100 to $300",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": "$300 to $500",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than $500",
+        "array": [300, 1500000]
+    }
+]
+
+
+
+const category = [
+    {
+        "_id": 1,
+        "name": "Food"
+    },
+    {
+        "_id": 2,
+        "name": "Baking"
+    },
+    {
+        "_id": 3,
+        "name": "Seasoning"
+    },
+    {
+        "_id": 4,
+        "name": "Cookware"
+    },
+    {
+        "_id": 5,
+        "name": "Spatula"
+    },
+    {
+        "_id": 6,
+        "name": "Knife"
+    },
+    {
+        "_id": 7,
+        "name": "Kitchen Appliances"
+    }
+]
+
+
+export {
+    price,
+    category
+}
